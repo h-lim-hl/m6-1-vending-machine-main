233fb6a61abb6825cff2df6816ebcf6f3bb63a26
Haris <greencrystallineeagle@gmail.com>
2025-07-01 17:39:05 +0800

    complete

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function main() {
   let machineValue = 0; // total amount in cents in the machine
-  let machineCreditDisplay = document.querySelector("#display #amount");
-  let machineItemDisplay = document.querySelector("#display #selected-item");
+  let machineDisplay = document.querySelector("#display");
 
-  function updateMachineCreditDisplay(amount = 0) {
-    machineCreditDisplay.textContent = `$ ${amount.toFixed(2)}`;
+  function updateMachineCreditDisplay(amount = machineValue) {
+     machineDisplay.querySelector("#amount").textContent = `$ ${(amount / 100.0).toFixed(2)}`;
   }
 
   function updateMachineItemDisplay(name = "SELECT AN ITEM") {
-    machineItemDisplay.textContent = name;
+    machineDisplay.querySelector("#selected-item").textContent = name;
   }
 
   function updateMachineCreditItemDisplay(amount, name) {
@@ -16,80 +15,136 @@ function main() {
     updateMachineItemDisplay(name);
   }
 
-  function updateItemStock (element, num) {
-    element.dataset.stock = num;
-    element.querySelector(".item-stock").textContent = num;
+  function resetMachineCreditItemDisplay() {
+    machineDisplay.classList.remove("fade-out");
+    updateMachineCreditDisplay();
+    updateMachineItemDisplay();
   }
 
-  function itemSelected (element, name, cost, stock) {
-
+  function machineDisplayTimedReset(mSec = 1000) {
+    setTimeout(resetMachineCreditItemDisplay, mSec);
   }
 
-  function getCentsAmountFromTextContext (element) {
+  function updateItemStock(element, num) {
+    console.log(element);
+    element.dataset.itemStock = num;
+    element.querySelector(".item-stock").textContent = `Stock: ${num}`;
+  }
+
+  function getItemButtonElement(childElement) {
+    const ITEM_CLASS = "item";
+    if (!(childElement instanceof HTMLElement)) {
+      console.error(`Param ${element} is not a HTMLElement`);
+      return null;
+    }
+
+    while (!childElement.classList.contains(ITEM_CLASS)) {
+      if (childElement.parentElement == null) {
+        console.error(`Could not find element with class ${ITEM_CLASS} in decendant tree`);
+        return null;
+      }
+      childElement = childElement.parentElement;
+    }
+    return childElement;
+  }
+
+  function itemSelected(element) {
+    element = getItemButtonElement(element);
+
+    const ITEM_NAME = element.dataset.itemName;
+    const ITEM_COST = element.dataset.itemCost;
+    const ITEM_STOCK = element.dataset.itemStock;
+
+    if (machineValue < ITEM_COST) {
+      updateMachineCreditItemDisplay(machineValue - ITEM_COST, ITEM_NAME);
+      machineDisplayTimedReset();
+    } else if (0 < ITEM_STOCK) {
+      updateItemStock(element, ITEM_STOCK - 1);
+      machineValue -= ITEM_COST;
+      
+      const DISPLAY_INNERHTML = machineDisplay.innerHTML;
+      machineDisplay.innerHTML = "Dispensing Product";
+      machineDisplay.classList.add("fade-out");
+      setTimeout(function(innerHtml) {
+        machineDisplay.innerHTML = DISPLAY_INNERHTML;
+        resetMachineCreditItemDisplay();
+      }, 3000, DISPLAY_INNERHTML);
+    }
+  }
+
+  function getCentsAmountFromTextContext(element) {
     const TEXT = element.textContent;
-    
     // Check if input is a price tag
     const CENTS_NOTATION = 1;
     const DOLLOR_NOTATION = 2;
     let notationFormat = 3;
 
-    if(!TEXT.includes("¢")) notationFormat ^= CENTS_NOTATION;
-    if(!TEXT.includes("$")) notationFormat ^= DOLLOR_NOTATION; 
+    if (!TEXT.includes("¢")) notationFormat ^= CENTS_NOTATION;
+    if (!TEXT.includes("$")) notationFormat ^= DOLLOR_NOTATION;
 
-    if(notationFormat == 0) {
+    if (notationFormat == 0) {
       console.error(`Element ${element} does not have valid price tag in it's textContext`);
       return null;
     }
 
     let matches = TEXT.match(/\b\d*\.?\d+\b/g);
-       
-    if(notationFormat === CENTS_NOTATION) {
+
+    if (notationFormat === CENTS_NOTATION) {
       return parseInt(matches[0]);
     }
 
-    let value = parseInt(matches[0]) * 100;
-    if (matches.length === 2) {
-      value += parseInt(matches[1]); 
-    }
+    return Math.floor(parseFloat(matches[0] * 100));
 
-    return value;
+  }
+
+  function getStockAmountFromTextContext(element) {
+    const TEXT = element.textContent;
+    const matches = TEXT.match(/Stock:\s*(\d+)/);
+    if (matches.length === 2) {
+      return parseInt(matches[1]);
+    }
+    console.error(`Element ${element} does not have a valid formated stock text`);
+    return null;
   }
 
   function initInsertCoinButtons() {
     let coinButtons = document.querySelectorAll(".coin-button");
-    coinButtons.forEach(function(button) {
+    coinButtons.forEach(function (button) {
       button.dataset.amount = getCentsAmountFromTextContext(button);
-      button.addEventListener("click", function(event) {
-        machineValue += button.dataset.amount;
+      button.addEventListener("click", function (event) {
+        machineValue += parseInt(button.dataset.amount);
+        updateMachineCreditDisplay(machineValue);
       });
     });
   }
 
   function initItemButtons() {
     let itemButtons = document.querySelectorAll(".items-section .item");
-    itemButtons.forEach(function(button){
+    itemButtons.forEach(function (button) {
       button.dataset.itemName = button.querySelector(".item-name").textContent;
-      button.dataset.itemCost = 
+      button.dataset.itemCost =
         getCentsAmountFromTextContext(button.querySelector(".item-price"));
-      button.dataset.stock = 
-        getCentsAmountFromTextContext(button.querySelector(".item-stock"));
-      
-      button.addEventListener("click", function(event){
-        const element = event.target;
-        console.log(event);
-        console.log(element);
+      button.dataset.itemStock =
+        getStockAmountFromTextContext(button.querySelector(".item-stock"));
 
-        itemSelected(
-          element,
-          element.dataset.itemName,
-          element.dataset.itemCost,
-          element.dataset.stock);
+      button.addEventListener("click", function (event) {
+        const element = event.target;
+        itemSelected(element);
       });
     });
 
   }
 
+  function initCancelButton() {
+    let cancelButton = document.querySelector("#cancel");
+    cancelButton.addEventListener("click", function () {
+      machineValue = 0;
+      resetMachineCreditItemDisplay();
+    });
+  }
+
   initInsertCoinButtons();
   initItemButtons();
+  initCancelButton();
 }
 main();
